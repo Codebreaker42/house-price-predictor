e613d0b757c47172a214d8093363db6f296f24d7
Nitin Budhlakoti <113322720+Codebreaker42@users.noreply.github.com>
2025-05-03 17:48:13 +0530

    data ingestion successfully completed

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,5 +1,6 @@
 import logging
 import os 
+import pandas as pd
 
 # log directory creation 
 log_dir="logs"
@@ -26,3 +27,43 @@ logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
 logger.debug("logging starts here")
+
+def load_data(data_url: str) -> pd.DataFrame:
+    """ load data from a csv file and change it into pd dataframe """
+    try:
+        df= pd.read_csv(data_url)
+        logger.debug(f'Data loaded from {data_url}')
+        return df
+    except pd.errors.ParseError as e:
+        logger.error(f"failed to parse the csv file {e}")
+        raise 
+    except Exception as e:
+        logger.error(f'Unexpected error occured while loading the data : {e}')
+        raise 
+
+def preprocess_data(df: pd.DataFrame) -> None:
+    """ removing the useless columns and saving csv file"""
+    try:
+        df.drop(columns=['Unnamed: 0','additionalrooms','carpetarea','opensides','facing','totalfloor','overlooking','projectname','possesiondate','roadfaceing'],inplace=True)
+        data_path= 'dataset'
+        os.makedirs(data_path)
+        dataset= os.path.join(data_path, 'main_data.csv')
+        df.to_csv(dataset)
+        logger.debug(f"dataset saved to {dataset} folder successfully")
+    except Exception as e:
+        logger.error(f'Unexpected error occur while saving the data {e}')
+        raise
+
+def main():
+    try:
+        data_path= 'https://raw.githubusercontent.com/Codebreaker42/house-price-predictor/refs/heads/master/Pune_property_data.csv'
+        df= load_data(data_url= data_path)
+        logger.debug("data loaded succesfully")
+        preprocess_data(df)
+    except Exception as e:
+        logger.error(f'Unexpected error occured while saving the file : {e}')
+
+
+
+if __name__ == '__main__':
+    main()
