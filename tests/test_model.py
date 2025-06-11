8f423faa076a9dcc2cd71e3f5c2bbbfefdebf2dd
Nitin Budhlakoti <113322720+Codebreaker42@users.noreply.github.com>
2025-06-11 17:57:01 +0530

    CICD- 18

---
+++ tests/test_model.py
@@ -0,0 +1,17 @@
+import unittest
+import mlflow
+import os
+import dagshub 
+
+class TestModelLoading(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        mlflow.set_tracking_uri('https://dagshub.com/nitinbdkt777/house-price-predictor.mlflow')
+        dagshub.init(repo_owner= "nitinbdkt777", repo_name="house-price-predictor")
+
+        model_name= "Xgboost"
+        # load the new model from MLflow model registry 
+        client = mlflow.MlflowClient()
+
+        prod_version = client.get_latest_versions(model_name, stages=['Production'])
+        
