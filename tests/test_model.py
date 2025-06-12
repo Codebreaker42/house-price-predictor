b74c65e0bb56caa4b17dc13f788dd96d04d97702
Nitin Budhlakoti <113322720+Codebreaker42@users.noreply.github.com>
2025-06-12 15:43:00 +0530

    CICD 22

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,18 @@
-# tests/test_dummy.py
 import unittest
+import mlflow
+import dagshub
 
-class DummyTest(unittest.TestCase):
-    def test_true(self):
-        self.assertTrue(True)
+class TestModelLoading(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        mlflow.set_tracking_uri('https://dagshub.com/nitinbdkt777/house-price-predictor.mlflow')
+        dagshub.init(repo_owner="nitinbdkt777", repo_name="house-price-predictor")
+        cls.client = mlflow.MlflowClient()
+        cls.model_name = "Xgboost"
+
+    def test_model_loaded_from_production(self):
+        prod_version = self.client.get_latest_versions(self.model_name, stages=['Production'])
+        self.assertTrue(len(prod_version) > 0, "No model in Production stage")
+
+if __name__ == "__main__":
+    unittest.main()
