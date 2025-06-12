e67a623daa37e3520efad37088e3cfa662f9def3
Nitin Budhlakoti <113322720+Codebreaker42@users.noreply.github.com>
2025-06-12 14:51:27 +0530

    CICD- 19

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,17 +1,15 @@
-import unittest
+# tests/test_model.py
+
 import mlflow
 import os
-import dagshub 
+import dagshub
 
-class TestModelLoading(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        mlflow.set_tracking_uri('https://dagshub.com/nitinbdkt777/house-price-predictor.mlflow')
-        dagshub.init(repo_owner= "nitinbdkt777", repo_name="house-price-predictor")
+def test_model_loading():
+    mlflow.set_tracking_uri('https://dagshub.com/nitinbdkt777/house-price-predictor.mlflow')
+    dagshub.init(repo_owner="nitinbdkt777", repo_name="house-price-predictor")
 
-        model_name= "Xgboost"
-        # load the new model from MLflow model registry 
-        client = mlflow.MlflowClient()
+    model_name = "Xgboost"
+    client = mlflow.MlflowClient()
+    prod_version = client.get_latest_versions(model_name, stages=['Production'])
 
-        prod_version = client.get_latest_versions(model_name, stages=['Production'])
-        
+    assert prod_version is not None
