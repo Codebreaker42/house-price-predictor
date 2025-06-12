2449fdaf69f817463f6b32e4617598be072de48b
Nitin Budhlakoti <113322720+Codebreaker42@users.noreply.github.com>
2025-06-12 17:02:09 +0530

    adding mlflow user name and password

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,26 +1,38 @@
-import os
+import unittest
 import mlflow
 import dagshub
-import unittest
+import os
 
 class TestModelLoading(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        mlflow.set_tracking_uri("https://dagshub.com/nitinbdkt777/house-price-predictor.mlflow")
+        # Below code block is for production use
+        # -------------------------------------------------------------------------------------
+        # Set up DagsHub credentials for MLflow tracking
+        dagshub_token = os.getenv("CAPSTONE_TEST")
+        if not dagshub_token:
+            raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
 
-        token = os.getenv("CAPSTONE_TEST")
-        if not token:
-            raise EnvironmentError("CAPSTONE_TEST is not set in environment.")
-    
-        os.environ["CAPSTONE_TEST"] = token
-        dagshub.init(repo_owner="nitinbdkt777", repo_name="house-price-predictor")
+        os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+        os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-        client = mlflow.MlflowClient()
-        prod_version = client.get_latest_versions("Xgboost", stages=["Production"])
-        print(prod_version)
+        dagshub_url = "https://dagshub.com"
+        repo_owner = "nitinbdkt777"
+        repo_name = "house-price-predictor"
+        # Set up MLflow tracking URI
+        mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+        cls.client= mlflow.MlflowClient()
 
+        # local use 
+        # ------------------------------------------------------------------------------------------
+        # mlflow.set_tracking_uri('https://dagshub.com/nitinbdkt777/house-price-predictor.mlflow')
+        # dagshub.init(repo_owner="nitinbdkt777", repo_name="house-price-predictor")
+        
+        
+    @staticmethod
     def test_model_loaded_from_production(self):
-        self.assertTrue(True)  # Add meaningful assertions here
+        prod_version = self.client.get_latest_versions(self.model_name, stages=['Production'])
+        self.assertTrue(len(prod_version) > 0, "No model in Production stage")
 
 if __name__ == "__main__":
     unittest.main()
