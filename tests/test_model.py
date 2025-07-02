983afb0bd1454e4fa84fd1fd71539c47c93d85a0
Nitin Budhlakoti <113322720+Codebreaker42@users.noreply.github.com>
2025-07-02 22:55:50 +0530

    adding the test cases

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,21 +7,21 @@ class TestModelLoading(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         pass
-        # # Below code block is for production use
-        # # -------------------------------------------------------------------------------------
-        # # Set up DagsHub credentials for MLflow tracking
-        # dagshub_token = os.getenv("HOUSE_PRICE_TOKEN")
-        # # if not dagshub_token:
-        # #     raise EnvironmentError("HOUSE_PRICE_TOKEN environment variable is not set")
+        # Below code block is for production use
+        # -------------------------------------------------------------------------------------
+        # Set up DagsHub credentials for MLflow tracking
+        dagshub_token = os.getenv("HOUSE_PRICE_TOKEN")
+        if not dagshub_token:
+            raise EnvironmentError("HOUSE_PRICE_TOKEN environment variable is not set")
 
-        # os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-        # os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+        os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+        os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-        # dagshub_url = "https://dagshub.com"
-        # repo_owner = "nitinbdkt777"
-        # repo_name = "house-price-predictor"
-        # # Set up MLflow tracking URI
-        # mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+        dagshub_url = "https://dagshub.com"
+        repo_owner = "nitinbdkt777"
+        repo_name = "house-price-predictor"
+        # Set up MLflow tracking URI
+        mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
         cls.client= mlflow.MlflowClient()
 
         # local use 
@@ -33,10 +33,8 @@ class TestModelLoading(unittest.TestCase):
         
     # @staticmethod
     def test_model_loaded_from_production(self):
-        self.client
-        # pass
-        # prod_version = self.client.get_latest_versions(self.model_name, stages=['Production'])
-        # self.assertTrue(len(prod_version) > 0, "No model in Production stage")
+        prod_version = self.client.get_latest_versions(self.model_name, stages=['Production'])
+        self.assertTrue(len(prod_version) > 0, "No model in Production stage")
 
 if __name__ == "__main__":
     unittest.main()
